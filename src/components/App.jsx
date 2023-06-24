b26ab54af96258750cad7b818e1bc975ccfbb7a7
Łukasz Kocela <lukaszkocela@gmail.com>
2023-06-24 19:47:17 +0200

    1stv

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,108 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { fetchQuery } from './services/api';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
+import { Modal } from './Modal/Modal';
+
+export class App extends Component {
+  state = {
+    photos: [],
+    searchValue: '',
+    page: 1,
+    error: null,
+    isLoading: false,
+    modal: '',
+  };
+
+  async componentDidUpdate(prevState, prevProps) {
+    if (
+      this.state.searchValue !== prevProps.searchValue ||
+      this.state.page !== prevProps.page
+    ) {
+      try {
+        this.setState({ isLoading: true });
+
+        const photos = await fetchQuery(
+          this.state.searchValue,
+          this.state.page
+        );
+
+        photos.map(photo => {
+          return this.setState(prevState => ({
+            photos: [
+              ...prevState.photos,
+              {
+                id: photo.id,
+                webformatURL: photo.webformatURL,
+                largeImageURL: photo.largeImageURL,
+                tags: photo.tags,
+              },
+            ],
+          }));
+        });
+      } catch (error) {
+        this.setState({ error });
+        console.log(this.state.error);
+      } finally {
+        this.setState({ isLoading: false });
+      }
+    }
+  }
+
+  searchValue = evt => this.setState({ photos: [], searchValue: evt });
+
+  showPhotos = () => {
+    const { photos } = this.state;
+    return photos;
+  };
+
+  handleLoadBtn = () => {
+    if (this.state.photos.length < 12) return 'none';
+  };
+
+  loadMorePhotos = evt => {
+    if (evt) {
+      this.setState({ page: this.state.page + 1 });
+
+      setTimeout(() => {
+        window.scrollTo({
+          top: document.body.scrollHeight,
+          behavior: 'smooth',
+        });
+      }, 500);
+    }
+  };
+
+  handleModal = imgShow => this.setState({ modal: imgShow });
+
+  modalClose = evt => this.setState({ modal: evt });
+
+  photosIntoModal = () => this.state.modal;
+
+  render() {
+    return (
+      <>
+        <Searchbar onSubmit={this.searchValue} />
+        <ImageGallery
+          photos={this.showPhotos()}
+          imgShow={this.handleModal}
+        />
+        {this.state.isLoading && <Loader />}
+        <div
+          className="BtnContainer"
+          style={{ display: this.handleLoadBtn() }}
+        >
+          {!this.state.isLoading && <Button onClick={this.loadMorePhotos} />}
+        </div>
+        {this.state.modal !== '' && (
+          <Modal
+            imgShow={this.photosIntoModal()}
+            onClick={this.modalClose}
+          />
+        )}
+      </>
+    );
+  }
+}
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,14 @@
+import { Radio } from 'react-loader-spinner';
+
+export const Loader = () => {
+  return (
+    <Radio
+  visible={true}
+  height="80"
+  width="80"
+  ariaLabel="radio-loading"
+  wrapperStyle={{}}
+  wrapperClass="Loader"
+    />
+  );
+};
